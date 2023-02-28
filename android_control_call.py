351b23324470d671d379c29e6828c163ae1407a4
donald <jianyu_a_wang2019@gmail.com>
2023-02-28 17:44:37 +0800

    修改：增加查询手机对应的微信并打电话的方法

--- android_control_call.py
+++ android_control_call.py
@@ -1,19 +1,36 @@
 import uiautomator2 as u2
 from method import delay
-c = u2.connect_usb('e697a1a5')
-# c = u2.connect_adb_wifi('192.168.0.100')
-# c = u2.connect('192.168.0.100:5555')
-c.app_start('com.tencent.mm')
-delay(3)
-c.click(0.93, 0.069)
+import os
 
-# # 点击底部加号按钮
-# c(resourceId="com.tencent.mm:id/aqe").click()
-# # 点击添加朋友选项
-# c(text="添加朋友").click()
-# # 在搜索框中输入微信号或手机号码
-# c(resourceId="com.tencent.mm:id/bhn").set_text("要添加的好友微信号或手机号码")
-# # 点击搜索按钮
-# c(resourceId="com.tencent.mm:id/kh").click()
-delay(3)
-c.app_stop('com.tencent.mm')
+
+def check_phone_and_call(info_number):
+    c = u2.connect_usb('e697a1a5')
+    c.app_start('com.tencent.mm')
+    delay(3)
+    x = input('微信是否已经打开输入框(回车是已经打开了)：')
+    if x != '':
+        print('没打开')
+        c.click(0.926, 0.072)  # 点击+号
+        c(text='添加朋友').click(offset=(0.5, 0.5))  # 点击添加朋友
+        c.click(0.384, 0.127)  # 点击空白行
+    else:
+        pass
+    c.send_keys(info_number, clear=True)  # 输入电话号
+    delay(3)
+    c.click(0.411, 0.138)  # 点击搜索
+    delay(5)
+    c.click(0.053, 0.072)  # 返回
+    delay(2)
+    c.click(0.811, 0.074)  # 清除
+    delay(1)
+    c.press("home")
+    # 使用ADB命令拨打电话
+    a = input('是否呼叫：')
+    if a == '':
+        os.system(f'adb shell am start -a android.intent.action.CALL -d tel:{info_number}')
+    else:
+        print('结束')
+
+
+phone = '13644120346'
+check_phone_and_call(phone)
